5f77d30863db84e1a7d0136d98bc07bcc73bc3cf
Somil Gupta <somilg050@gmail.com>
2020-10-17 01:33:53 +0530

    added new client impl

--- lambda/index.js
+++ lambda/index.js
@@ -92,6 +92,61 @@ var featureOptions = { method: 'GET',
    } 
 };
 
+var listOptions = { method: 'GET',
+  url: 'http://3.95.154.161/meetings/list',
+  qs: { pageno: '1', userid: 'thom.grace@traho.io' },
+  headers: 
+   { 'Postman-Token': '05dfa266-4136-479f-98eb-a8037d9c12cc',
+     'cache-control': 'no-cache',
+     Authorization: ''
+   }
+};
+
+var endOptions = { 
+  method: 'PUT',
+  url: 'http://3.95.154.161/meetings/start',
+  qs: { action: 'end' },
+  headers: { 
+     Authorization: '',
+     'Content-Type': 'application/json'
+  },
+  body: '',
+  json: true 
+};
+
+var startOptions = { 
+  method: 'PUT',
+  url: 'http://3.95.154.161/meetings/start',
+  qs: { action: 'start' },
+  headers: { 
+     Authorization: '',
+     'Content-Type': 'application/json'
+  },
+  body: '',
+  json: true 
+};
+
+var createOptions = { method: 'POST',
+  url: 'http://3.95.154.161/meetings/create',
+  qs: { userid: 'thom.grace@traho.io' },
+  headers: 
+   { 'Postman-Token': 'b1fc8247-cf9c-427e-8105-54697bca371e',
+     'cache-control': 'no-cache',
+     'Content-Type': 'application/json' },
+  body: 
+   { meeting_type: 2,
+     duration: 10,
+     start_time: '2020-05-13T13:00:00Z',
+     topic: 'Special Test Meetings',
+     recurrence_type: 1,
+     repeat_interval: 5,
+     invitees: 
+      [ 'First_name Last_name',
+        'First_name Last_name',
+        'First_name Last_name',
+        'First_name Last_name' ] },
+  json: true };
+
 async function auth(options) {
   return new Promise((resolve) => {
     request(options, function (error, response, body) {
@@ -129,6 +184,178 @@ const LaunchRequest = {
       .reprompt(speechOutput)
       .getResponse();
   }
+};
+
+const CreateMeeting = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'createMeeting';
+  },
+  async handle(handlerInput) {
+    const SessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    var speechOutput = '';
+
+    if(!SessionAttributes.LoginStatus){
+      await auth(loginOptions).then((response) => {
+        response = JSON.parse(response);
+        SessionAttributes.LoginStatus = response;
+      });
+    }
+    if(!SessionAttributes.MeetingList){
+      listOptions.headers.Authorization = SessionAttributes.LoginStatus.token;
+      await auth(listOptions).then((response) => {
+        response = JSON.parse(response);
+        SessionAttributes.MeetingList = response;
+      });
+    }
+
+    
+    
+    SessionAttributes.Last = speechOutput;
+    
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(speechOutput)
+      .getResponse();
+  },
+};
+
+const StartMeeting = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'startMeeting';
+  },
+  async handle(handlerInput) {
+    const SessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    var speechOutput = '';
+
+    if(!SessionAttributes.LoginStatus){
+      await auth(loginOptions).then((response) => {
+        response = JSON.parse(response);
+        SessionAttributes.LoginStatus = response;
+      });
+    }
+    if(!SessionAttributes.MeetingList){
+      listOptions.headers.Authorization = SessionAttributes.LoginStatus.token;
+      await auth(listOptions).then((response) => {
+        response = JSON.parse(response);
+        SessionAttributes.MeetingList = response;
+      });
+    }
+
+    var len = SessionAttributes.MeetingList.meetings.length;
+    console.log(SessionAttributes.MeetingList.meetings.length);
+    if(len === 0){
+      speechOutput = `You don't have any meetings for today on your calender. What else can I help you with? `;
+    }
+    else{
+      startOptions.headers.Authorization = SessionAttributes.LoginStatus.token;
+      startOptions.body = SessionAttributes.MeetingList.meetings[0];
+      speechOutput = `${SessionAttributes.UserData.name}, your next meeting is on ${SessionAttributes.MeetingList.meetings[0].topic}. Would you like to start? `;
+      SessionAttributes.WaitStart = true;
+      SessionAttributes.StartOptions = startOptions;
+    }
+    
+    SessionAttributes.Last = speechOutput;
+    
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(speechOutput)
+      .getResponse();
+  },
+};
+
+const EndMeeting = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'endMeeting';
+  },
+  async handle(handlerInput) {
+    const SessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    var speechOutput = '';
+
+    if(!SessionAttributes.LoginStatus){
+      await auth(loginOptions).then((response) => {
+        response = JSON.parse(response);
+        SessionAttributes.LoginStatus = response;
+      });
+    }
+    if(!SessionAttributes.MeetingList){
+      listOptions.headers.Authorization = SessionAttributes.LoginStatus.token;
+      await auth(listOptions).then((response) => {
+        response = JSON.parse(response);
+        SessionAttributes.MeetingList = response;
+      });
+    }
+
+    var len = SessionAttributes.MeetingList.meetings.length;
+    console.log(SessionAttributes.MeetingList.meetings.length);
+    if(len === 0){
+      speechOutput = `You don't have any meetings for today on your calender. What else can I help you with? `;
+    }
+    else{
+      endOptions.headers.Authorization = SessionAttributes.LoginStatus.token;
+      endOptions.body = SessionAttributes.MeetingList.meetings[0];
+      speechOutput = `${SessionAttributes.UserData.name}, you want to end your meeting on ${SessionAttributes.MeetingList.meetings[0].topic}? `;
+      SessionAttributes.WaitEnd = true;
+      SessionAttributes.EndOptions = endOptions;
+    }
+    
+    SessionAttributes.Last = speechOutput;
+    
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(speechOutput)
+      .getResponse();
+  },
+};
+
+const ListMeeting = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'listMeeting';
+  },
+  async handle(handlerInput) {
+    const SessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    
+    if(!SessionAttributes.LoginStatus){
+      await auth(loginOptions).then((response) => {
+        response = JSON.parse(response);
+        SessionAttributes.LoginStatus = response;
+      });
+    }
+    
+    listOptions.headers.Authorization = SessionAttributes.LoginStatus.token;
+    
+    await auth(listOptions).then((response) => {
+      response = JSON.parse(response);
+      SessionAttributes.MeetingList = response;
+    });
+
+    var speechOutput;
+    var len = SessionAttributes.MeetingList.meetings.length;
+    console.log(SessionAttributes.MeetingList.meetings.length);
+    if(len === 0){
+      speechOutput = `You don't have any meetings for today on your calender.`;
+    }
+    else{
+      speechOutput = `So here's the detail of your meetings in next 24 hours: ` ;
+      
+      for(var i=0; i<len; i++){
+        var meetingDate = new Date(SessionAttributes.MeetingList.meetings[i].start_time).toLocaleString("en-US", {timeZone: "America/New_York"});
+        meetingDate = meetingDate.toString().replace(' GMT-0400 (Coordinated Universal Time)', '');
+        var topic = SessionAttributes.MeetingList.meetings[i].topic;
+        speechOutput += `<break time="1s"/>${topic} scheduled on ${meetingDate}. `;
+      }
+    }
+    
+    SessionAttributes.Last = speechOutput;
+    
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(speechOutput)
+      .getResponse();
+  },
 };
 
 const FeatureImplementation = {
@@ -201,9 +428,35 @@ const YesIntentHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'
       && SessionAttributes.ChangeHistory === true);
   },
-  handle(handlerInput){
+  async handle(handlerInput){
     const SessionAttributes = handlerInput.attributesManager.getSessionAttributes();
     var speechOutput = `${SessionAttributes.FeatureStatus.change_history} What else can I help you with?`;
+
+    if(SessionAttributes.WaitEnd === true){
+      await auth(endOptions).then((response) => {
+        console.log(response);
+      });
+      SessionAttributes.WaitEnd = false;
+      speechOutput = `Meeting ended. `;
+      return handlerInput.responseBuilder
+        .speak(speechOutput)
+        .reprompt(speechOutput)
+        .withShouldEndSession(true)
+        .getResponse();
+    }
+    
+    if(SessionAttributes.WaitStart === true){
+      await auth(startOptions).then((response) => {
+        console.log(response);
+      });
+      SessionAttributes.WaitStart = false;
+      speechOutput = `Meeting started. `;
+      return handlerInput.responseBuilder
+        .speak(speechOutput)
+        .reprompt(speechOutput)
+        .withShouldEndSession(true)
+        .getResponse();
+    }
 
     return handlerInput.responseBuilder
       .speak(speechOutput)
@@ -661,6 +914,10 @@ exports.handler = skillBuilder
     ProjectStatus,
     TrelloBoards,
     TrelloLists,
+    StartMeeting,
+    EndMeeting,
+    CreateMeeting,
+    ListMeeting,
     CreateTrelloTasks,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
